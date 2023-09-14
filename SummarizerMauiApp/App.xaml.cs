f0508e1528d59a36cc0b05998083e0b926b8ba77
SenalAriyaratne <ariyaratne.senal@gmail.com>
2023-09-14 22:04:58 +1000

    Add project files.

---
+++ SummarizerMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SummarizerMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SummarizerMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SummarizerMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
